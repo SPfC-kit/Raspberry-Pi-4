d8833c82c94f66b6242c0d50987c89b25dec4514
makoto <yoshioka.0209.06@gmail.com>
2025-08-26 18:05:15 +0900

    ピン番号修正しました

--- LED_Blinking.py
+++ LED_Blinking.py
@@ -7,17 +7,17 @@ import RPi.GPIO as GPIO
 # BCM(GPIO番号)で指定する設定
 GPIO.setmode(GPIO.BCM)
 
-# GPIO17を出力モード設定
+# GPIO26を出力モード設定
 GPIO.setup(26, GPIO.OUT)
 
 while True:
-  # GPIO17の出力を1にして、LED点灯
+  # GPIO26の出力を1にして、LED点灯
   GPIO.output(26, 1)
 
   # 0.5秒待つ
   time.sleep(0.5)
 
-  # GPIO17の出力を0にして、LED消灯
+  # GPIO26の出力を0にして、LED消灯
   GPIO.output(26, 0)
 
   # 0.5秒待つ
